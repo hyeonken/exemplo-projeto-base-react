d7749009935b6a4a6cca86a4ff79c642f5e371ad
Rony Ken <rm551549@fiap.com.br>
2023-08-21 09:55:27 -0300

    Import e estrutura basica react

---
+++ src/components/Menu.jsx
@@ -0,0 +1,13 @@
+
+export default function Menu() {
+  return (
+    
+    <ul>
+        <li>FIAP</li>
+        <li>GOOGLE</li>
+        <li>FACEBOOK</li>
+        <li>TWITTER</li>
+    </ul>
+
+  )
+}
